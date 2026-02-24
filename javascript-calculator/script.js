a05a137ef31605102dac004fea32465d124bacd9
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-02-23 21:41:58 -0800

    feat: initialize calculator variables with null

--- javascript-calculator/script.js
+++ javascript-calculator/script.js
@@ -1,20 +1,30 @@
 // ===== Basic Math Operation Functions =====
 
+// Adds two numbers
 function add(a, b) {
   return a + b;
 }
 
+// Subtracts second number from first number
 function subtract(a, b) {
   return a - b;
 }
 
+// Multiplies two numbers
 function multiply(a, b) {
   return a * b;
 }
 
+// Divides first number by second number
 function divide(a, b) {
   if (b === 0) {
     return "Don't divide by 0!";
   }
   return a / b;
 }
+
+// Variables to update display for later
+// Initilized as null to distinguish from 0
+let firstNumber = null; // No value yet
+let secondNumber = null; // No value yet
+let operator = null; // no operator yet
