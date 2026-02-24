366d9b8593861ab0998de6895adf344d67c4ca81
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-02-23 22:29:38 -0800

    feat: initialize calculator and add operate function

--- javascript-calculator/script.js
+++ javascript-calculator/script.js
@@ -23,8 +23,24 @@ function divide(a, b) {
   return a / b;
 }
 
+function divide(a, b) {
+  if (b === 0) {
+    return "Don't divide by 0!";
+  } else {
+    return a / b;
+  }
+}
+
 // Variables to update display for later
 // Initilized as null to distinguish from 0
 let firstNumber = null; // No value yet
 let secondNumber = null; // No value yet
 let operator = null; // no operator yet
+
+//
+function operate(operator, a, b) {
+  if (operator === "+") return a + b;
+  if (operator === "-") return a - b;
+  if (operator === "*") return a * b;
+  if (operator === "/") return a / b;
+}
